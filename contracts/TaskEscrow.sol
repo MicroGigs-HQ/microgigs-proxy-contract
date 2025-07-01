2532d5108462698474f263edc999748fac107e00
shaaibu7 <shaaibusuleiman9@gmail.com>
2025-07-01 14:17:54 +0100

    chore: project setup for proxy contract

---
+++ contracts/TaskEscrow.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract TaskEscrow is Initializable {
+    
+}
---
+++ contracts/TaskFactory.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "./TaskEscrow.sol";
+
+contract TaskFactory is Initializable {}
